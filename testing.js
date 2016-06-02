4c69af1537ef1282bc0c5883e17f26a934e10152
Michelle Quaglia <mqsydney@gmail.com>
2016-06-02 17:24:21 +1000

    no message

--- testing.js
+++ testing.js
@@ -8,9 +8,16 @@ function prepareEventHandlers(){
 	var speaker = document.getElementById("speakerIcon");
 	var audio=new Audio('AC_Word Identification.mp3');
 
+
 	speaker.onclick=function(){
 		audio.play();
 	}
+
+	myButton.onclick=function{
+		myButton class="changeBackground";
+	}
+
+
 }
 
 window.onload=function(){
