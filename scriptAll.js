8de7df14245f50a2db4fe806836bd0abf9c8b00a
Michelle Quaglia <mqsydney@gmail.com>
2016-06-04 12:30:21 +1000

    Adding in AC-Spanish and also Spanish menu

---
+++ scriptAll.js
@@ -0,0 +1,50 @@
+function prepareEventHandlers(){
+	
+	var forkButton=document.getElementById("fork");
+	var myButton=document.getElementById("ratButton");
+	var quest=document.getElementById("question");
+	var speaker=document.getElementById("speakerIcon");
+	var menu=document.getElementById("menu");
+	var skipSpanishSpoken1=document.getElementById("skip");
+	var submit=document.getElementById("submit");
+	var eng=document.getElementById("eng");
+	var span=document.getElementById("span");
+	var login=document.getElementById("loginButt");
+
+	var audio=new Audio('AC_Spanish_Spoken_1.mp3');
+	var perro=new Audio('perro.mp3');
+
+
+login.onclick=function(){
+	if(span.checked){
+		alert("hello");
+		window.location.assign("https://sdsdevx.github.io/menuSpanish.html")
+	}
+	
+}
+
+	speaker.onclick=function(){
+		perro.play();
+	}
+
+	quest.onclick=function(){
+		audio.play();
+	}
+	
+	menu.onclick=function(){
+		window.location.assign("https://sdsdevx.github.io/menu.html")
+	}
+
+	skipSpanishSpoken1.onclick=function(){
+		window.location.assign("https://sdsdevx.github.io/menu.html")
+	}
+
+	menu.onclick=function(){
+		window.location.assign("https://sdsdevx.github.io/menu.html")
+	}
+
+}
+
+window.onload=function(){
+	prepareEventHandlers();
+}
