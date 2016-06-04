4ec9b5880a381ce551b5d618d0fbf99109ec0680
Xi Zhong <Xi Zhong>
2016-06-05 04:57:34 +1000

    cleaned up the storage/made the login button save and link

--- xi.js
+++ xi.js
@@ -1,41 +1,117 @@
-var question = document.getElementById("qname").innerHTML;
-var correctimage = document.getElementById("correct");
-var pic1 = document.getElementById("pic1");
-var pic2 = document.getElementById("pic2");
-var pic3 = document.getElementById("pic3");
-var SPname = document.getElementById("SPname");
-var Pname = document.getElementById("Pname")
-var loginButt = document.getElementById("loginButt");
+var question = $('#qname').html();
+//var correctimage = document.getElementById("correct");
+//var pic1 = document.getElementById("pic1");
+//var pic2 = document.getElementById("pic2");
+//var pic3 = document.getElementById("pic3");
+//var SPname = document.getElementById("SPname");
+//var Pname = document.getElementById("Pname");
+//var loginButt = document.getElementById("loginButt");
 
-correctimage.onclick = function() {
-		localStorage.setItem(question, "correct");
+
+var output = "";
+for(var key in sessionStorage) { 
+    output += key+"\n";
+    output += sessionStorage[key]+"\n";
+    output += "\n";
+}
+
+$('#sendmail').click(function() {
+	var output = "";
+for(var key in sessionStorage) { 
+    output += key+"\n";
+    output += sessionStorage[key]+"\n";
+    output += "\n";
+	
+	var link = "mailto:xi_zhong@live.com"
+	+ "?cc="
+	+ "&subject=testing"
+	+ "&body=" + output;
+});
+
+//<a href='mailto:user@domain?subject=[subject here]&body=[email body here]'></a>
+
+//<a href='javascript:sendLocalStorageByEmail(prompt("Please enter your e-mail address"))'>
+   //Send Email
+//</a>
+
+
+/**function sendLocalStorageByEmail(recipient) {
+    // create localstorage string
+    var output = "";
+    for(var key in sessionStorage) { 
+        output += key+"\n";
+        output += sessionStorage[key]+"\n";
+        output += "\n";
+    }
+
+    // create temporary anchor to emulate mailto click in new tab
+    var anchor = document.createElement("a");
+    anchor.href = "mailto:"+recipient+"?subject=Local+Storage+Data&body="+encodeURIComponent(output);
+    anchor.style.display = "none"; 
+    anchor.setAttribute("target","_blank");
+    anchor.appendChild(document.createTextNode(""));
+    document.body.appendChild(anchor);
+
+    if (anchor.click) {
+        return anchor.click();
+    }
+
+    // some browsers (chromium/linux) have trouble with anchor.click
+    var clickEv = document.createEvent("HTMLEvents");
+    clickEv.initEvent("click", true, true);
+    anchor.dispatchEvent(clickEv)
+}**/
+$('#correct').click(function(){
+	sessionStorage.setItem(question, "correct");
+});
+
+$('#pic1').click(function(){
+	sessionStorage.setItem(question, "incorrect");
+});
+
+$('#pic2').click(function(){
+	sessionStorage.setItem(question, "incorrect");
+});
+
+$('#pic3').click(function(){
+	sessionStorage.setItem(question, "incorrect");
+});
+
+$('#storage').click(function(){
+	var SPname = $('#SPname');
+	var Pname = $('#Pname');
+	sessionStorage.setItem("SPname", SPname.val());
+	sessionStorage.setItem("Pname", Pname.val());
+	window.location.href = 'menu.html';
+});
+
+$('#loginButt').click(function(){
+	window.location.href = 'menu.html';
+});
+/**function right(){
+	var correctimage = document.getElementById("correct");
+	sessionStorage.setItem(question, "correct");
 	}
 				
-pic1.onclick = function() {
-		localStorage.setItem(question, "incorrect");
+function wrong() {
+	var pic1 = document.getElementById("pic1");
+	sessionStorage.setItem(question, "incorrect");
 	}
 	
 pic2.onclick = function() {
-		localStorage.setItem(question, "incorrect");
+	var pic2 = document.getElementById("pic2");
+	sessionStorage.setItem(question, "incorrect");
 	}
 	
 pic3.onclick = function() {
-		localStorage.setItem(question, "incorrect");
-	}
-//loginButt.onclick=function(){
-	//localStorage.setItem("SPname", SPname.value);
-	//ocalStorage.setItem("Pname", Pname.value);
-	//window.location.assign("https://sdsdevx.github.io/menu.html");
-//
-function save() {
-	localStorage.setItem("SPname", SPname.value);
-	localStorage.setItem("Pname", Pname.value);
-	
-	
-}
+	var pic3 = document.getElementById("pic3");
+	sessionStorage.setItem(question, "incorrect");
 
-//loginButton.onclick=function(){
-	//localStorage.setItem("SPname", SPname.value);
-	//localStorage.setItem("Pname", Pname.value);
-////var incorrectimage = document.getElementByClass("incorrect")
-//incorrectimage.onclick = localStorage.setItem("question", "incorrect")
+
+function save() {
+	var SPname = document.getElementById("SPname");
+	var Pname = document.getElementById("Pname");
+	sessionStorage.setItem("SPname", SPname.value);
+	sessionStorage.setItem("Pname", Pname.value);
+}
+**/
