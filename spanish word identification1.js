0d02a63fdea90fc2684a4a08e961ac1802eb9ab0
Michelle Quaglia <mqsydney@gmail.com>
2016-06-09 14:34:36 +1000

    spanish word ID 1

---
+++ spanish word identification1.js
@@ -0,0 +1,25 @@
+function prepareEventHandlers(){
+	
+	var forkButton=document.getElementById("fork");
+	var myButton=document.getElementById("ratButton");
+
+	var question=document.getElementById("question");
+	var perro=new Audio("tenedor.mp3");
+
+	var speaker = document.getElementById("speakerIcon");
+	var audio=new Audio('spanish wrod identification 1.mp3');
+
+
+	speaker.onclick=function(){
+		audio.play();
+	}
+
+	question.onclick=function(){
+		perro.play();
+	}
+
+}
+
+window.onload=function(){
+	prepareEventHandlers();
+}
