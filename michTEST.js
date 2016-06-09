6ff282fdc1a0218f8801bd9dd749ff800926eeed
Michelle Quaglia <mqsydney@gmail.com>
2016-06-09 11:07:49 +1000

    still debugging spanish page

--- michTEST.js
+++ michTEST.js
@@ -1,12 +1,13 @@
 function prepareEventHandlers(){
 	
-	var forkButton=document.getElementById("fork");
-	var myButton=document.getElementById("ratButton");
-
+	//var forkButton=document.getElementById("fork");
+	//var myButton=document.getElementById("ratButton");
+	var question = document.getElementById("question");
 	
 
 	var speaker = document.getElementById("speakerIcon");
 	var audio=new Audio('AC_Spanish_Spoken_1.mp3');
+	var perro = new Audio('perro.mp3')
 
 
 	speaker.onclick=function(){
@@ -14,6 +15,9 @@ function prepareEventHandlers(){
 	}
 
 }
+	question.onclick=function(){
+		perro.play();
+	}
 
 window.onload=function(){
 	prepareEventHandlers();
