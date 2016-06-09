cfac767198be86551a05f71d9f43408dd436ceed
Michelle Quaglia <mqsydney@gmail.com>
2016-06-09 11:42:13 +1000

    no message

--- michTEST.js
+++ michTEST.js
@@ -3,13 +3,18 @@ function prepareEventHandlers(){
 	//var forkButton=document.getElementById("fork");
 	//var myButton=document.getElementById("ratButton");
 	var question = document.getElementById("question");
-
-	
-
 	var speaker = document.getElementById("speakerIcon");
 	var audio=new Audio('AC_Spanish_Spoken_1.mp3');
 	var perro = new Audio('perro.mp3');
 
+	var skip = document.getElementById("skip");
+	skip.onclick=function(){
+		window.location.assign("https://sdsdevx.github.io/menuSpanish.html")
+	}
+	var submit=document.getElementById("submit");
+	submit.onclick=function(){
+		window.location.assign("https://sdsdevx.github.io/menuSpanish.html")
+	}
 
 	question.onclick=function(){
 		audio.play();
